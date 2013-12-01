9ee8033a968f2601a1e73fa5969303d691dd67f4
Brian Sam-Bodden <bsbodden@integrallis.com>
2013-11-30 21:08:50 -0700

    Added Ember Data and a simple BlogPost model

--- javascripts/app/ember-blog-app.js
+++ javascripts/app/ember-blog-app.js
@@ -1,5 +1,12 @@
+// Ember application object
 EmberBlog = Ember.Application.create({
   ready: function() {
     console.log('Ember is ready!');
   }
+});
+
+// A simple client-side model for a blog post
+EmberBlog.BlogPost = DS.Model.extend({
+  title: DS.attr('string'),
+  body: DS.attr('string')
 });
